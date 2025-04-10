170c2a552ca4a32c5cbeaa32c24b8b4102167e73
56usernotfound <saadsaleem835@gmail.com>
2025-04-10 12:34:04 +0500

    first commit

---
+++ main.dart
@@ -0,0 +1,21 @@
+void main() {
+  String Name = 'Saad Saleem';
+  String Gender = 'Male';
+  int Age = 19;
+  String Call = '03151047945';
+  String Email = 'saadsaleem835@gmail.com';
+  bool isworking = false;
+  bool isstudent = true;
+  String Feild = 'Software Engineering';
+  String Languages = "C,Dart,Javascript";
+
+  print('Name:$Name');
+  print('Gender;$Gender');
+  print('Age:$Age');
+  print('Call:$Call');
+  print('Email:$Email');
+  print('Working:$isworking');
+  print('Student:$isstudent');
+  print('Feild:$Feild');
+  print("Languages Fimilair with:$Languages");
+}
